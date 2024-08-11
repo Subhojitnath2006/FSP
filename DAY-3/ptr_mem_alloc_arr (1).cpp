e03a08bbf78f3f9dda5a4c53420d882071632e90
Subhojit <rabinath750@gmail.com>
2024-08-11 15:40:23 +0530

    pinters

--- DAY-3/ptr_mem_alloc_arr (1).cpp
+++ DAY-3/ptr_mem_alloc_arr (1).cpp
@@ -1,29 +1,35 @@
 #include <iostream>
 using namespace std;
-
+int y=6;
+void f2(){
+    cout<<"why omg?";
+    int x;
+    
+}
 int main() {
-    int num;
-    cout << "Enter total number of students: ";
-    cin >> num;
-    float* pMarks;
+    // int num;
+    // cout << "Enter total number of students: ";
+    // cin >> num;
+    // float* pMarks;
     
-    // memory allocation of num number of floats
-    pMarks = new float[num];
+    // // memory allocation of num number of floats
+    // pMarks = new float[num];
     
-    cout << "Enter marks of students." << endl;
-    for (int i = 0; i < num; ++i) {
-        cout << "Student" << i + 1 << ": ";
-        cin >> *(pMarks + i);
-    }
+    // cout << "Enter marks of students." << endl;
+    // for (int i = 0; i < num; ++i) {
+    //     cout << "Student" << i + 1 << ": ";
+    //     cin >> *(pMarks + i);
+    // }
     
-    cout << "\nDisplaying marks of students." << endl;
-    for (int i = 0; i < num; ++i) {
-    cout << "Student" << i + 1 << ": " << *(pMarks + i) << endl;
-    }
+    // cout << "\nDisplaying marks of students." << endl;
+    // for (int i = 0; i < num; ++i) {
+    // cout << "Student" << i + 1 << ": " << *(pMarks + i) << endl;
+    // }
     
-    // ptrMarks memory is released
-    delete[] pMarks;
-    pMarks = nullptr;
-    
+    // // ptrMarks memory is released
+    // delete[] pMarks;
+    // pMarks = nullptr;
+
+    int a;
     return 0;
 }
