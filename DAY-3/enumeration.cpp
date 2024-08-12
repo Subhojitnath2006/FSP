3d97484a612e29c56d7b8a3d2f0f38a45c96f107
Subhojit <rabinath750@gmail.com>
2024-08-12 10:37:10 +0530

    enumeration

---
+++ DAY-3/enumeration.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+enum colour{red,green,blue};
+
+int main()
+{
+    Day today;
+    today = Wednesday; //we can only store the constants declared under enum day
+    cout<<sizeof(today)<<endl; //size is 4 byte because in today we can only store one constant at a time
+    cout << "Day " << today+1;
+    return 0;
+}
